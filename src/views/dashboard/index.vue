1afe96f20b843fc924ec8f215a8b13539d42d13f
“wanxiaoyu” <2224374779@qq.com>
2022-01-10 17:33:35 +0800

    提交信息

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- <div>
+      <ul v-for="set in sets">
+        <li v-for="n in even(set)">{{ n }}</li>
+      </ul>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dashboard",
+  // data() {
+  //   return {
+  //     sets: [
+  //       [1, 2, 3, 4, 5],
+  //       [6, 7, 8, 9, 10],
+  //     ],
+  //   };
+  // },
+  // methods: {
+  //   even: function (numbers) {
+  //     return numbers.filter(function (number) {
+  //       return number % 2 === 0;
+  //     });
+  //   },
+  // },
+  computed: {
+    ...mapGetters(["name"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
